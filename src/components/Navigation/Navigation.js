0568b6aa7c90d8b4df7ea277527b6ff4e988898c
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-02 18:13:23 +0200

    Styling navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,11 +6,12 @@ class Navigation extends Component {
     render() {
         //Mapping through navigation elements passed in props
         const navButtons = this.props.navItems.map((element, index) => {
-            return <NavLink key={index} className={'navButton'} to={element.link}>{element.item}</NavLink>
+            return <NavLink key={index} className={'navButton'} activeStyle={{ color: 'dodgerblue' }} to={element.link}>{element.item}</NavLink>
         });
 
         return (
             <nav className={'navBar'}>
+                <NavLink className={'logo'} to={'/top100'}><span className={'logo-top100'}>Top 100</span><span>albums</span></NavLink>
                 {navButtons}
             </nav>
         );
