43f753f0d1c23c782b89d0ecf05d5cd50a8f8c55
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-02 02:00:37 +0200

    Adding fetch

--- src/components/TopAlbums/TopAlbums.js
+++ src/components/TopAlbums/TopAlbums.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './TopAlbums.scss';
 
 class TopAlbums extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
+            .then(response => response.json())
+            .then(response => this.setState({ data: response.feed.entry }));
+
+    }
+
     render() {
+        console.log(this.state.data);
         return (
-            <div>Albums</div>
+            <div></div>
         )
     }
 }
