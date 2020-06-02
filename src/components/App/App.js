2986b6b77e3a5a673f5d967be7c1bfef21907f70
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-03 01:53:07 +0200

    Removing ComingSoon component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.scss';
 import { BrowserRouter, Redirect, Switch, Route } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
-import ComingSoon from "../ComingSoon/ComingSoon";
 import TopAlbums from "../TopAlbums/TopAlbums";
 import SideDrawer from "../SideDrawer/SideDrawer";
 import Backdrop from "../Backdrop/Backdrop";
@@ -37,17 +36,10 @@ class App extends Component {
     };
 
     render() {
-        //Array with navigation elements
-        const navItems = [
-            { item: 'Top 100 albums', link: '/top100' },
-            { item: 'News', link: '/news' },
-            { item: 'Photos', link: '/photos' },
-            { item: 'Video', link: '/video' },
-            { item: 'Contact', link: '/contact' },
-        ];
+        const navItems = [];
         //Adding route path to ComingSoon element to future tabs
         const comingSoonItems = navItems.map((element, index) => {
-            return index >= 1 ? <Route path={element.link} component={ComingSoon} key={index} /> : null
+            return index >= 1 ? <Route path={element.link} key={index} /> : null
         });
 
         let backdrop;
