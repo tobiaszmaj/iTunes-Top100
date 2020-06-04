1c4f1d9f88b5fb1ed6b72d72a041dfbf52066c96
Tobiasz Maj <tobiaszmaj1@gmail.com>
2020-06-05 00:39:54 +0200

    Revert "Changing Route path"
    This reverts commit 6f5d2e5ebc4aa9b3a14835b50fd3fc66a9d4a3ff.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -37,6 +37,7 @@ class App extends Component {
 
     render() {
         const navItems = [];
+        //Adding route path to ComingSoon element to future tabs
         const comingSoonItems = navItems.map((element, index) => {
             return index >= 1 ? <Route path={element.link} key={index} /> : null
         });
@@ -57,7 +58,7 @@ class App extends Component {
                         <Switch>
                             <Redirect exact from="/" to="/top100" />
                             {comingSoonItems}
-                            <Route path={process.env.PUBLIC_URL + '/'} component={TopAlbums} />
+                            <Route path={'/top100'} component={TopAlbums} />
                             <Route component={NotFound} />
                         </Switch>
                     </div>
